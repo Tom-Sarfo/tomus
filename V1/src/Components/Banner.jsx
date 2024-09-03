4016cde56db566f8bfc1a6e87a3453f5a1e135c9
Tom-Sarfo <tomsarfodavis@gmail.com>
2024-09-03 19:50:31 +0000

    Add navbar and banner template

---
+++ V1/src/Components/Banner.jsx
@@ -0,0 +1,19 @@
+import Box from "@mui/material/Box";
+
+function Banner() {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "280px",
+        textAlign: "center",
+        padding: "2rem",
+        backgroundColor: "#D9D9D9",
+      }}
+    >
+      this is a paper
+    </Box>
+  );
+}
+
+export default Banner;
